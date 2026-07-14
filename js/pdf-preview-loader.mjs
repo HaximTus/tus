0e73c33fb3de240cc6dfc53fe0e59fb299fd9891
Haxim <tjiux2007@gmail.com>
2026-07-14 17:45:32 +0800

    Remove unused streaming PDF loader

--- js/pdf-preview-loader.mjs
+++
@@ -1,5 +0,0 @@
-import * as pdfjsLib from './pdf.min.mjs';
-
-pdfjsLib.GlobalWorkerOptions.workerSrc = new URL('./pdf.worker.min.mjs', import.meta.url).href;
-window.TusPdfPreview = pdfjsLib;
-window.dispatchEvent(new Event('tus:pdf-ready'));
